40a7f62c2979e8aaac261be32554cddd5ecb040b
Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
2014-09-29 09:42:19 +0200

    [DRIVERS] Backlight and PWM backlight API added.
    This patch adds the backlight and the PWM driven backlight device support,
    and the backlight device used by i.MX6-based Nitrogen6x (this should be moved
    to a dedicated device tree file).

--- drivers/video/objects.mk
+++ drivers/video/objects.mk
@@ -48,3 +48,5 @@ drivers-objs-$(CONFIG_FB_ARMCLCD)+= video/amba-clcd.o
 drivers-objs-$(CONFIG_FB_VESA)+= video/vesafb.o
 drivers-objs-$(CONFIG_FB_MXC) += video/mxcfb.o
 drivers-objs-$(CONFIG_FB_MXC_LDB) += video/mxcldb.o
+drivers-objs-$(CONFIG_FB_BACKLIGHT) += video/backlight/backlight.o
+drivers-objs-$(CONFIG_FB_BACKLIGHT_PWM) += video/backlight/pwm_bl.o
